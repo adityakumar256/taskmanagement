ef4be8c2fb145b262733e7f7a3f95e3eeb1c7157
Aditya Kumar <aditya.95080@gmail.com>
2026-01-11 17:33:58 +0530

    added

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,7 +3,10 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   name: String,
   email: { type: String, unique: true },
-  password: String
+  password:{
+    type:String,
+    required:true
+  }
 });
 
 module.exports = mongoose.model("User", userSchema);
